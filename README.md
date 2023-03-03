1fc6fc3881bb8d0ae2e83650875dfeddb409f8bd
Aurelien Macbook <aurelien.roger1@gmail.com>
2023-03-03 23:10:47 +0100

    Controle des champs de formulaire de leur value

--- README.md
+++ README.md
@@ -11,3 +11,17 @@
 - Installation de Sementic Ui react
 - Installation de Fomentic Ui pour palier le bug de Sementic Ui
 
+## Etape 2
+
+### Intégration d'un formulaire
+
+- Intégration d'un formulaire simplifié avec 2 champs:
+ - Salaire Mensuel Brut
+ - Salaire Mensuel Net
+
+## Etape 3
+
+### Control des champs de formulaire
+
+- Controle du champs en Brut
+-Controle du champs en Net
--- src/components/FormConvert/FormConvert.js
+++ src/components/FormConvert/FormConvert.js
@@ -1,16 +1,38 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-unresolved */
-import React from 'react';
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Form } from 'semantic-ui-react';
 
 function FormConvert() {
+  const [brut, setBrut] = useState(0);
+  const [net, setNet] = useState(0);
+
+  const handleChangeValueBrut = (event) => {
+    setBrut(event.target.value);
+  };
+
+  const handleChangeValueNet = (event) => {
+    setNet(event.target.value);
+  };
+
+  const calculSalaire = () => {
+
+  };
+
   return (
     <Form>
       <Form.Field>
-        <input placeholder="Salaire Mensuel Brut" />
+        <label htmlFor="brut">
+          Salaire Mensuel Brut
+          <input placeholder="Salaire Mensuel Brut" value={brut} name="brut" onChange={handleChangeValueBrut} />
+        </label>
       </Form.Field>
       <Form.Field>
-        <input placeholder="Salaire Mensuel Net" />
+        <label htmlFor="net">
+          Salaire Mensuel Net
+          <input placeholder="Salaire Mensuel Net" value={net} onChange={handleChangeValueNet} />
+        </label>
+
       </Form.Field>
     </Form>
   );
