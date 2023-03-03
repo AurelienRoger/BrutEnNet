69452874d2e93913fa6025ababf46552b7ac46b6
Aurelien Macbook <aurelien.roger1@gmail.com>
2023-03-03 22:56:41 +0100

    Intégration formulaire simplifié

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # BrutEnNet
+
+## Etape 1
+
+### Installation d'un react model
+
+- Installation de React
+
+### Installation de Sementic UI React
+
+- Installation de Sementic Ui react
+- Installation de Fomentic Ui pour palier le bug de Sementic Ui
+
--- src/components/FormConvert/FormConvert.js
+++ src/components/FormConvert/FormConvert.js
@@ -7,17 +7,11 @@ function FormConvert() {
   return (
     <Form>
       <Form.Field>
-        <label>First Name</label>
-        <input placeholder="First Name" />
+        <input placeholder="Salaire Mensuel Brut" />
       </Form.Field>
       <Form.Field>
-        <label>Last Name</label>
-        <input placeholder="Last Name" />
+        <input placeholder="Salaire Mensuel Net" />
       </Form.Field>
-      <Form.Field>
-        <Checkbox label="I agree to the Terms and Conditions" />
-      </Form.Field>
-      <Button type="submit">Submit</Button>
     </Form>
   );
 }
