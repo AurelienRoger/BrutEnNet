4e66958103e5b864ffa8bd6421e631ede9a767fe
Aurelien Macbook <aurelien.roger1@gmail.com>
2023-03-05 15:34:09 +0100

    Ajout du form horaire brut et net

--- src/components/FormConvert/FormConvert.js
+++ src/components/FormConvert/FormConvert.js
@@ -47,19 +47,36 @@ function FormConvert() {
 
   return (
     <Form>
-      <Form.Field>
-        <label htmlFor="brut">
-          Salaire Mensuel Brut
-          <input type="number" placeholder="Salaire Mensuel Brut" value={brut} name="brut" onChange={handleChangeValueBrut} />
-        </label>
-      </Form.Field>
-      <Form.Field>
-        <label htmlFor="net">
-          Salaire Mensuel Net
-          <input type="number" placeholder="Salaire Mensuel Net" value={net} onChange={handleChangeValueNet} />
-        </label>
+      <Form.Group widths="equal">
+        <Form.Field>
+          <label htmlFor="brut">
+            Salaire Mensuel Brut
+            <input type="number" placeholder="Salaire Mensuel Brut" value={brut} name="brut" onChange={handleChangeValueBrut} />
+          </label>
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="net">
+            Salaire Mensuel Net
+            <input type="number" placeholder="Salaire Mensuel Net" value={net} onChange={handleChangeValueNet} />
+          </label>
 
-      </Form.Field>
+        </Form.Field>
+      </Form.Group>
+      <Form.Group widths="equal">
+        <Form.Field>
+          <label htmlFor="horairebrut">
+            Salaire Horaire Brut
+            <input type="number" placeholder="Salaire Horaire Brut" value={brut} name="horairebrut" onChange={handleChangeValueBrut} />
+          </label>
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="horairenet">
+            Salaire Horaire Net
+            <input type="number" placeholder="Salaire Horaire Net" value={net} name="horairenet" onChange={handleChangeValueNet} />
+          </label>
+
+        </Form.Field>
+      </Form.Group>
     </Form>
   );
 }
