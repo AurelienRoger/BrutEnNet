fb13c2290ed98f45b7acc1d6d3181ffbcc4bb499
Aurelien Macbook <aurelien.roger1@gmail.com>
2023-03-07 23:02:23 +0100

    Design salaire apres impots

--- src/components/FormImpot/FormImpot.js
+++ src/components/FormImpot/FormImpot.js
@@ -1,40 +1,36 @@
 import { useState, useEffect } from 'react';
 import '../App/styles.css';
-import { Form } from 'semantic-ui-react';
+import { List, Label } from 'semantic-ui-react';
 
 function FormImpot({ net, slider, setNet }) {
   const [netImpot, setNetImpot] = useState(0);
   const [annuelNetImpot, setAnnuelNetImpot] = useState(0);
 
   const calculNetApresImpots = (salaireNet, percentImpot) => {
-    const result = (salaireNet - ((salaireNet * percentImpot) / 100)).toFixed(2);
+    const result = (salaireNet - Math.round((salaireNet * percentImpot) / 100));
+    const resultAnnuel = Math.round(result * 12);
     setNetImpot(result);
-  };
-
-  const handleChangeMensuelImpot = (event) => {
-    setNetImpot(event.target.value);
+    setAnnuelNetImpot(resultAnnuel);
   };
 
   useEffect(() => {
     calculNetApresImpots(net, slider);
   }, [net, slider]);
   return (
-    <Form className="formConvert">
-      <Form.Group widths="equal">
-        <Form.Field>
-          <label htmlFor="brut">
-            Mensuel Net après Impots
-            <input disabled type="number" placeholder="Salaire Mensuel Brut" value={netImpot} name="brut" onChange={handleChangeMensuelImpot} />
-          </label>
-        </Form.Field>
-        <Form.Field>
-          <label htmlFor="brut">
-            Annuel Net après Impots
-            <input type="number" placeholder="Salaire Mensuel Brut" value={annuelNetImpot} name="brut" />
-          </label>
-        </Form.Field>
-      </Form.Group>
-    </Form>
+    <List divided selection>
+      <List.Item>
+        <Label color="olive" horizontal>
+          {netImpot}
+        </Label>
+        Mensuel Net après impots
+      </List.Item>
+      <List.Item>
+        <Label color="olive" horizontal>
+          {annuelNetImpot}
+        </Label>
+        Annuel Net après impots
+      </List.Item>
+    </List>
   );
 }
 
